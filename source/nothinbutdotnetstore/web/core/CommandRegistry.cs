9360a91b884c8cbfd56184ad7cd3167739f607f9
Gary G <gary.gallagher@allscripts.com>
2011-09-15 11:29:21 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -1,12 +1,24 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 namespace nothinbutdotnetstore.web.core
 {
     public class CommandRegistry : ICanFindCommands
     {
+        IEnumerable<IProcessARequest> all_commands;
+
+        public CommandRegistry(IEnumerable<IProcessARequest> all_command)
+        {
+            this.all_commands = all_commands; 
+        }
+
 
         public IProcessARequest get_the_command_that_can_process(IContainRequestInformation the_request)
         {
-           return this.get_the_command_that_can_process(the_request);          
+            foreach (var command in all_commands)
+            {
+                if (command == the_request)
+                    return command;
+            }
            
         }
     }
